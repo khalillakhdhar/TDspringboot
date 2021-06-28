18d3dfd97973431f2225cb4eabb844318c886cf6
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-28 15:50:13 +0100

    Update CustomerController.java

--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -1,5 +1,25 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("users")
 public class CustomerController {
+@GetMapping
+public String getUsers()
+{
+	return "<h2>afficher la <a href='/liste'> liste</a></h2>";
+	
+}
+@GetMapping("/liste")
+public ModelAndView getpage() 
+{
+	ModelAndView mv=new ModelAndView("liste.html");
+	return mv;
+}
 
 }
